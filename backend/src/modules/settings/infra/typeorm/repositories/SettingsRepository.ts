bc83ef9541ac4cebc99e00292a71ccb00f9fbf62
Gui-dev <adrianon10@gmail.com>
2021-04-27 19:51:27 -0300

    refactor: add check user already exists

--- backend/src/modules/settings/infra/typeorm/repositories/SettingsRepository.ts
+++ backend/src/modules/settings/infra/typeorm/repositories/SettingsRepository.ts
@@ -3,10 +3,18 @@ import { getRepository } from 'typeorm'
 import { Setting } from '@modules/settings/infra/typeorm/entities/Setting'
 import { ISettingsRepository } from '@modules/settings/repositories/ISettingsRepository'
 import { ISettingsReposytoryDTO } from '@modules/settings/dtos/ISettingsReposytoryDTO'
+import { AppError } from '@shared/error/AppError'
 
 export class SettingsRepository implements ISettingsRepository {
   public async create ({ username, chat }: ISettingsReposytoryDTO): Promise<Setting> {
     const settingsRepository = getRepository(Setting)
+
+    const userAlreadyExists = await settingsRepository.findOne({ username })
+
+    if (userAlreadyExists) {
+      throw new AppError('User already exists')
+    }
+
     const settings = settingsRepository.create({ username, chat })
     await settingsRepository.save(settings)
 

3c7c87b6ab74191544992feae8f5dcea6f9b3d2f
Gui-dev <adrianon10@gmail.com>
2021-04-28 03:08:38 -0300

    feat: add routes users.routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import settingsRoutes from '@modules/settings/infra/http/routes/settings.routes'
+import usersRoutes from '@modules/users/infra/http/routes/users.routes'
 
 const routes = Router()
 
 routes.use('/settings', settingsRoutes)
+routes.use('/users', usersRoutes)
 
 export default routes

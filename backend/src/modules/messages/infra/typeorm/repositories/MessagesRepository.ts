35706be9e12bfdf88f08cba5b29c24d346c473e1
Gui-dev <adrianon10@gmail.com>
2021-04-29 02:58:20 -0300

    feat: add new helper method listByUser

--- backend/src/modules/messages/infra/typeorm/repositories/MessagesRepository.ts
+++ backend/src/modules/messages/infra/typeorm/repositories/MessagesRepository.ts
@@ -1,20 +1,34 @@
-import { getRepository } from 'typeorm'
+import { getRepository, Repository } from 'typeorm'
 
 import { IMessagesRepository } from '@modules/messages/repositories/IMessagesRepository'
 import { Message } from '../entities/Message'
 import { IMessagesRepositoryDTO } from '@modules/messages/dtos/IMessagesRepositoryDTO'
 
 export class MessagesRepository implements IMessagesRepository {
+  private messagesRepository: Repository<Message>
+
+  constructor () {
+    this.messagesRepository = getRepository(Message)
+  }
+
   public async create ({ admin_id, user_id, text }: IMessagesRepositoryDTO): Promise<Message> {
-    const messagesRepository = getRepository(Message)
-    const message = messagesRepository.create({
+    const message = this.messagesRepository.create({
       admin_id,
       user_id,
       text
     })
 
-    await messagesRepository.save(message)
+    await this.messagesRepository.save(message)
 
     return message
+  }
+
+  public async listByUser (user_id: string): Promise<Message[]> {
+    const list = await this.messagesRepository.find({
+      where: { user_id },
+      relations: ['user']
+    })
+
+    return list
   }
 }

60a7167a6873af8215fbf613ea3e0496169bbf33
Gui-dev <adrianon10@gmail.com>
2021-04-29 02:59:04 -0300

    feat: add new helper method index

--- backend/src/modules/messages/infra/http/controllers/MessagesController.ts
+++ backend/src/modules/messages/infra/http/controllers/MessagesController.ts
@@ -2,6 +2,14 @@ import { Request, Response } from 'express'
 import { MessagesRepository } from '../../typeorm/repositories/MessagesRepository'
 
 export class MessagesController {
+  public async index (request: Request, response:Response): Promise<Response> {
+    const { user_id } = request.params
+
+    const messagesRepository = new MessagesRepository()
+    const listMessages = await messagesRepository.listByUser(user_id)
+    return response.status(201).json(listMessages)
+  }
+
   public async create (request: Request, response:Response): Promise<Response> {
     const { admin_id, user_id, text } = request.body
 

100e947d04aac49ce06e89ee2dfe8a3a4f74c1ad
Gui-dev <adrianon10@gmail.com>
2021-04-29 02:59:46 -0300

    feat: add new route get /messages/:user_id

--- backend/src/modules/messages/infra/http/routes/messages.routes.ts
+++ backend/src/modules/messages/infra/http/routes/messages.routes.ts
@@ -5,6 +5,7 @@ import { MessagesController } from '@modules/messages/infra/http/controllers/Mes
 const messagesRoutes = Router()
 const messagesController = new MessagesController()
 
+messagesRoutes.get('/:user_id', messagesController.index)
 messagesRoutes.post('/', messagesController.create)
 
 export default messagesRoutes

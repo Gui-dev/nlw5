f3f4acfb927db0c500f15470dc9a2a42bd3c8232
Gui-dev <adrianon10@gmail.com>
2021-05-04 19:49:56 -0300

    feat: add interface helper method findByUsername

--- backend/src/modules/settings/repositories/ISettingsRepository.ts
+++ backend/src/modules/settings/repositories/ISettingsRepository.ts
@@ -2,5 +2,6 @@ import { ISettingsReposytoryDTO } from '@modules/settings/dtos/ISettingsReposyto
 import { Setting } from '@modules/settings/infra/typeorm/entities/Setting'
 
 export interface ISettingsRepository {
-  create({ username, chat }: ISettingsReposytoryDTO): Promise<Setting>
+  create ({ username, chat }: ISettingsReposytoryDTO): Promise<Setting>
+  findByUsername (username: string): Promise<Setting>
 }

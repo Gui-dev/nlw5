35aa324fac055db12b6472f8bd5ac2fef283a3c1
Gui-dev <adrianon10@gmail.com>
2021-05-06 03:34:24 -0300

    feat: add interface IConnectionRepository and helper methods create, findByUserId interfaces

---
+++ backend/src/modules/connections/repositories/IConnectionRepository.ts
@@ -0,0 +1,7 @@
+import { IConnectionRepositoryDTO } from '../dtos/IConnectionRepositoryDTO'
+import { Connection } from '../infra/typeorm/entities/Connection'
+
+export interface IConnectionRepository {
+  create (data: IConnectionRepositoryDTO): Promise<Connection>
+  findByUserId (user_id: string): Promise<Connection>
+}

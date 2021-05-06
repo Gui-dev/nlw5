570bf2819fff3ffeb78002d05b8714a422843c32
Gui-dev <adrianon10@gmail.com>
2021-05-06 03:31:20 -0300

    feat: add connections with socket.io

--- backend/public/js/chat.js
+++ backend/public/js/chat.js
@@ -1,1 +1,24 @@
-document.querySelector("#start_chat").addEventListener("click", (event) => {});
+document.querySelector('#start_chat').addEventListener('click', (event) => {
+  // eslint-disable-next-line no-undef
+  const socket = io('http://localhost:3333')
+
+  const chatHelp = document.getElementById('chat_help')
+  chatHelp.style.display = 'none'
+
+  const chatInSupport = document.getElementById('chat_in_support')
+  chatInSupport.style.display = 'block'
+
+  const email = document.getElementById('email').value
+  const text = document.getElementById('txt_help').value
+
+  socket.on('connect', () => {
+    const params = { email, text }
+    socket.emit('client_first_access', params, (callback, err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(callback)
+      }
+    })
+  })
+})
---
+++ backend/src/shared/infra/websockets/ClientService.ts
@@ -0,0 +1,50 @@
+import { Socket } from 'socket.io'
+
+import { io } from '@shared/infra/http/Http'
+import { ConnectionRepository } from '@modules/connections/infra/typeorm/repositories/ConnectionRepository'
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository'
+import { MessagesRepository } from '@modules/messages/infra/typeorm/repositories/MessagesRepository'
+
+interface IParams {
+  text: string
+  email: string
+}
+
+io.on('connect', (socket: Socket) => {
+  socket.on('client_first_access', async (params: IParams) => {
+    const connectionRepository = new ConnectionRepository()
+    const usersRepository = new UsersRepository()
+    const messagesRepository = new MessagesRepository()
+
+    const socket_id = socket.id
+    const { text, email } = params as IParams
+
+    const userExists = await usersRepository.findByEmail(email)
+
+    if (!userExists) {
+      const user = await usersRepository.create(email)
+
+      await connectionRepository.create({
+        user_id: user.id,
+        socket_id
+      })
+    }
+
+    const connection = await connectionRepository.findByUserId(userExists.id)
+
+    if (!connection) {
+      await connectionRepository.create({
+        user_id: userExists.id,
+        socket_id
+      })
+    }
+
+    connection.socket_id = socket_id
+    await connectionRepository.create(connection)
+
+    await messagesRepository.create({
+      user_id: userExists.id,
+      text
+    })
+  })
+})

5c96ba16c62a696672eedb68a3c711898ef420d5
Gui-dev <adrianon10@gmail.com>
2021-05-06 03:40:55 -0300

    feat: add new route put /settings/:username

--- backend/src/modules/settings/infra/http/routes/settings.routes.ts
+++ backend/src/modules/settings/infra/http/routes/settings.routes.ts
@@ -7,5 +7,6 @@ const settingsController = new SettingsController()
 
 settingsRoutes.get('/:username', settingsController.show)
 settingsRoutes.post('/', settingsController.create)
+settingsRoutes.put('/:username', settingsController.update)
 
 export default settingsRoutes

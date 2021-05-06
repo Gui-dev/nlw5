5ca62261d04387786d8a024d592658d775648a75
Gui-dev <adrianon10@gmail.com>
2021-05-06 03:39:04 -0300

    feat: add helper method interface update

--- backend/src/modules/settings/repositories/ISettingsRepository.ts
+++ backend/src/modules/settings/repositories/ISettingsRepository.ts
@@ -4,4 +4,5 @@ import { Setting } from '@modules/settings/infra/typeorm/entities/Setting'
 export interface ISettingsRepository {
   create ({ username, chat }: ISettingsReposytoryDTO): Promise<Setting>
   findByUsername (username: string): Promise<Setting>
+  update (username: string, chat: boolean): Promise<void>
 }

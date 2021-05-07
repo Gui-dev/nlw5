07888375c707ec2f792d0d09981d4043a459dbbc
Gui-dev <adrianon10@gmail.com>
2021-05-07 03:37:14 -0300

    feat: add fetching all messages with socket.io

--- backend/public/js/chat.js
+++ backend/public/js/chat.js
@@ -21,4 +21,29 @@ document.querySelector('#start_chat').addEventListener('click', (event) => {
       }
     })
   })
+
+  socket.on('client_list_all_messages', messages => {
+    const template_client = document.getElementById('message-user-template').innerHTML
+    const template_admin = document.getElementById('admin-template').innerHTML
+    const messagesDOM = document.getElementById('messages')
+
+    messages.forEach((message) => {
+      if (message.admin_id === null) {
+        // eslint-disable-next-line no-undef
+        const rendered = Mustache.render(template_client, {
+          message: message.text,
+          email
+        })
+
+        messagesDOM.innerHTML += rendered
+      } else {
+        // eslint-disable-next-line no-undef
+        const rendered = Mustache.render(template_admin, {
+          message_admin: message.text
+        })
+
+        messagesDOM.innerHTML += rendered
+      }
+    })
+  })
 })

39304c7ac775f2b856f1257cf171d70b0559d0df
Gui-dev <adrianon10@gmail.com>
2021-05-10 02:17:10 -0300

    feat: add interface helper method findAllWithoutAdmin

--- backend/src/modules/connections/repositories/IConnectionRepository.ts
+++ backend/src/modules/connections/repositories/IConnectionRepository.ts
@@ -4,4 +4,5 @@ import { Connection } from '../infra/typeorm/entities/Connection'
 export interface IConnectionRepository {
   create (data: IConnectionRepositoryDTO): Promise<Connection>
   findByUserId (user_id: string): Promise<Connection>
+  findAllWithoutAdmin (): Promise<Connection[]>
 }

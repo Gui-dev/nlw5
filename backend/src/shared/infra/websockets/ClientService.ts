c8a675cb29a1c02561eec0df9b18250b27a7b1d0
Gui-dev <adrianon10@gmail.com>
2021-05-12 02:51:03 -0300

    feat: add new socket message client_send_to_admin

--- backend/src/shared/infra/websockets/ClientService.ts
+++ backend/src/shared/infra/websockets/ClientService.ts
@@ -8,14 +8,15 @@ import { MessagesRepository } from '@modules/messages/infra/typeorm/repositories
 interface IParams {
   text: string
   email: string
+  socket_admin_id?: string
 }
 
 io.on('connect', (socket: Socket) => {
-  socket.on('client_first_access', async (params: IParams) => {
-    const connectionRepository = new ConnectionRepository()
-    const usersRepository = new UsersRepository()
-    const messagesRepository = new MessagesRepository()
+  const connectionRepository = new ConnectionRepository()
+  const usersRepository = new UsersRepository()
+  const messagesRepository = new MessagesRepository()
 
+  socket.on('client_first_access', async (params: IParams) => {
     const socket_id = socket.id
     const { text, email } = params as IParams
 
@@ -50,5 +51,19 @@ io.on('connect', (socket: Socket) => {
     const allMessages = await messagesRepository.listByUser(userExists.id)
 
     socket.emit('client_list_all_messages', allMessages)
+  })
+
+  socket.on('client_send_to_admin', async (params: IParams) => {
+    const { text, socket_admin_id } = params
+    const { user_id } = await connectionRepository.findBySocketId(socket.id)
+    const message = await messagesRepository.create({
+      user_id,
+      text
+    })
+
+    io.to(socket_admin_id).emit('admin_receive_message', {
+      message,
+      socket_id: socket.id
+    })
   })
 })

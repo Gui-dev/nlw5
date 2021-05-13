07cd397261baabd15db33457b8fa054a85c65a47
Gui-dev <adrianon10@gmail.com>
2021-05-13 19:43:37 -0300

    style: add breaking point 480px

--- podcastr/src/components/Header/style.ts
+++ podcastr/src/components/Header/style.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const Container = styled.header`
   display: flex;
   align-items: center;
-  /* justify-content: space-around; */
   padding: 2rem 4rem;
   height: 10.4rem;
   background-color: var(--white);
@@ -20,5 +19,28 @@ export const Container = styled.header`
     font-size: 1.6rem;
     margin-left: auto;
     text-transform: capitalize;
+  }
+
+  @media (max-width: 480px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem 2rem;
+
+    p {
+      font-size: 1.6rem;
+      margin-top: 0.5rem;
+      margin-bottom: 0.5rem;
+      padding: 0;
+      margin-left: 0;
+      border: 0;
+    }
+
+    span {
+      display: block;
+      font-size: 1.6rem;
+      margin-left: 0;
+      text-transform: capitalize;
+    }
   }
 `

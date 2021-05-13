82559353a3c1092109e6053fc7c478ddda7ff331
Gui-dev <adrianon10@gmail.com>
2021-05-13 03:24:14 -0300

    style: add global styles

--- podcastr/src/style/global.ts
+++ podcastr/src/style/global.ts
@@ -18,8 +18,50 @@ export const GlobalStyle = createGlobalStyle`
     --purple-800: #6F48C9;
   }
 
+  @media (max-width: 1080px) {
+    html, body {
+      font-size: 93.75%; // 15px
+    }
+  }
+
+  @media (max-width: 1080px) {
+    html, body {
+      font-size: 87.5%; // 14px
+    }
+  }
+
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    background-color: var(--gray-50);
+    color: var(--gray-500);
+  }
+
+  html, body, input, textarea, button {
+    font-size: 62.5%;
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Lexend', sans-serif;
+    font-weight: 600;
+    color: var(--gray-800);
+  }
+
+  h1 {
+    font-size: 3.2rem;
+  }
+
+  h2 {
+    font-size: 2.4rem;
+  }
+
+  button {
+    cursor: pointer;
   }
 `

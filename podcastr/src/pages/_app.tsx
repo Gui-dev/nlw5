8da091f0e21bc9a7add11dbadbe742eaa64671a8
Gui-dev <adrianon10@gmail.com>
2021-05-13 03:25:14 -0300

    feat: add app config

--- podcastr/src/pages/_app.tsx
+++ podcastr/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import { GlobalStyle } from '../style/global'
@@ -6,9 +5,6 @@ import { GlobalStyle } from '../style/global'
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Podcastr</title>
-      </Head>
       <GlobalStyle />
       <Component { ...pageProps }/>
     </>
--- podcastr/src/pages/_document.tsx
+++ podcastr/src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps (ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            sheet.collectStyles(<App {...props} />)
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -21,10 +21,26 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        )
       }
     } finally {
       sheet.seal()
     }
+  }
+
+  render () {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <title>Podcastr</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }

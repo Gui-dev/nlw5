a9aa9c31f0779530d6d2d0371083963f16406ba5
Gui-dev <adrianon10@gmail.com>
2021-05-14 03:13:34 -0300

    refactor: move component Header

--- podcastr/src/pages/_app.tsx
+++ podcastr/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from 'next/app'
 
 import { GlobalStyle } from '../style/global'
+import { Container } from '../style/app'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <GlobalStyle />
-      <Component { ...pageProps }/>
-    </>
+    <Container>
+      <main>
+        <GlobalStyle />
+        <Header />
+        <Component { ...pageProps }/>
+      </main>
+      <Player />
+    </Container>
   )
 }
--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,14 +1,11 @@
 import Head from 'next/head'
 
-import { Header } from '../components/Header'
-
 export default function Home () {
   return (
     <>
       <Head>
         <title>Podcastr</title>
       </Head>
-      <Header />
     </>
   )
 }

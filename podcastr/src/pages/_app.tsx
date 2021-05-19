bd931c9ab589346200da79728ad0bfb5aebf8172
Gui-dev <adrianon10@gmail.com>
2021-05-19 16:54:33 -0300

    feat: add context api PlayerProvider

--- podcastr/src/pages/_app.tsx
+++ podcastr/src/pages/_app.tsx
@@ -4,16 +4,19 @@ import { GlobalStyle } from '../style/global'
 import { Container } from '../style/app'
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
+import { PlayerProvider } from '../context/PlayerContext'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <main>
-        <GlobalStyle />
-        <Header />
-        <Component { ...pageProps }/>
-      </main>
-      <Player />
+      <PlayerProvider>
+        <main>
+          <GlobalStyle />
+          <Header />
+          <Component { ...pageProps }/>
+        </main>
+        <Player />
+      </PlayerProvider>
     </Container>
   )
 }

0d3bbbaecb108ea9186a8495e479351d4bf28f85
Gui-dev <adrianon10@gmail.com>
2021-05-28 03:38:26 -0300

    feat: add screen Welcome and google font

--- plant-manager/App.tsx
+++ plant-manager/App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import { View } from 'react-native'
+import AppLoading from 'expo-app-loading'
 
-export default function App() {
+import { Welcome } from './src/screens/Welcome'
+
+export default function App () {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <AppLoading />
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <>
       <StatusBar style="dark" />
-    </View>
-  );
+      <Welcome />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

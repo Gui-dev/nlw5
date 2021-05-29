94ccad9ce3d74ec9cbc100dc4c0e2dc73257e94e
Gui-dev <adrianon10@gmail.com>
2021-05-29 03:04:32 -0300

    style: add styles screen Welcome

--- plant-manager/src/screens/Welcome/style.ts
+++ plant-manager/src/screens/Welcome/style.ts
@@ -1,17 +1,20 @@
 import styled from 'styled-components/native'
 import { Dimensions } from 'react-native'
 import colors from './../../styles/colors'
+import fonts from './../../styles/fonts'
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
   justify-content: space-around;
   margin-top: 30px;
+  padding: 0 20px;
 `
 
 export const Title = styled.Text`
-  font-size: 32px;
-  font-weight: bold;
+  font-size: 28px;
+  font-family: ${fonts.heading};
+  line-height: 34px;
   text-align: center;
   color: ${colors.heading};
 `
@@ -22,6 +25,7 @@ export const Image = styled.Image`
 
 export const Description = styled.Text`
   font-size: 18px;
+  font-family: ${fonts.text};
   text-align: center;
   color: ${colors.heading};
   padding: 0 20px;
@@ -37,10 +41,4 @@ export const ButtonNext = styled.TouchableOpacity.attrs({
   margin-bottom: 10px;
   background-color: ${colors.green};
   border-radius: 16px;
-`
-
-export const ButtonNextText = styled.Text`
-  font-size: 24px;
-  text-align: center;
-  color: ${colors.white};
 `

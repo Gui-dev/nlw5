6f7717b35ed00baa6698f0f486c806b0d54065b3
Gui-dev <adrianon10@gmail.com>
2021-05-31 03:45:31 -0300

    feat: add functions handleInputBlur, handleInputFocus and handleInputChange

--- plant-manager/src/screens/UserIdentification/index.tsx
+++ plant-manager/src/screens/UserIdentification/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Platform } from 'react-native'
 
 import { Button } from '../../components/Button'
 import { Container, Wrapper, Content, Form, Header, Title, Emoji, Input } from './style'
 
 export const UserIdentification: React.FC = () => {
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [name, setName] = useState<string>()
+
+  const handleInputBlur = () => {
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+
+  const handleInputFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleInputChange = (value: string) => {
+    setIsFilled(!!value)
+    setName(value)
+  }
+
   return (
     <Container>
       <Wrapper
@@ -14,10 +32,17 @@ export const UserIdentification: React.FC = () => {
         <Content>
           <Form>
             <Header>
-              <Emoji>😊️</Emoji>
+              <Emoji>{ isFilled ? '😍' : '😃' }</Emoji>
               <Title>Como podemos{'\n'} chamar você ?</Title>
             </Header>
-            <Input placeholder="Digite seu nome"/>
+            <Input
+              placeholder="Digite seu nome"
+              onChangeText={ handleInputChange }
+              onBlur={ handleInputBlur }
+              onFocus={ handleInputFocus }
+              isFocused={ isFocused }
+              isFilled={ isFilled }
+            />
             <Button title="Confirmar"/>
           </Form>
         </Content>

e73f36b1c29123d51ff789282e9c6c7ad6e1b637
Gui-dev <adrianon10@gmail.com>
2021-06-01 03:34:39 -0300

    feat: add function handleNavigationToUserIdentification

--- plant-manager/src/screens/Welcome/index.tsx
+++ plant-manager/src/screens/Welcome/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 
 import logo from './../../assets/watering.png'
 import { Container, Title, Image, Description, ButtonNext } from './style'
 
 export const Welcome: React.FC = () => {
+  const { navigate } = useNavigation()
+
+  const handleNavigationToUserIdentification = () => {
+    navigate('UserIdentification')
+  }
+
   return (
     <Container>
       <Title>Gerencie{'\n'} suas plantas de{'\n'} forma fácil</Title>
@@ -18,7 +25,7 @@ export const Welcome: React.FC = () => {
         Nós cuidamos de lembrar você sempre que precisar
       </Description>
 
-      <ButtonNext>
+      <ButtonNext onPress={ handleNavigationToUserIdentification }>
         <Feather name="chevron-right" size={ 32 } color="#FFF"/>
       </ButtonNext>
     </Container>

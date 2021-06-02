8e79b7a53db29675f8c90449633e50fd9bb06b17
Gui-dev <adrianon10@gmail.com>
2021-06-02 01:23:05 -0300

    feat: add function handleNavigationToConfirmation

--- plant-manager/src/screens/UserIdentification/index.tsx
+++ plant-manager/src/screens/UserIdentification/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { Platform } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Keyboard, Platform, TouchableWithoutFeedback } from 'react-native'
 
 import { Button } from '../../components/Button'
 import { Container, Wrapper, Content, Form, Header, Title, Emoji, Input } from './style'
 
 export const UserIdentification: React.FC = () => {
+  const { navigate } = useNavigation()
   const [isFocused, setIsFocused] = useState(false)
   const [isFilled, setIsFilled] = useState(false)
   const [name, setName] = useState<string>()
@@ -23,29 +25,38 @@ export const UserIdentification: React.FC = () => {
     setName(value)
   }
 
+  const handleNavigationToConfirmation = () => {
+    navigate('Confirmation')
+  }
+
   return (
     <Container>
       <Wrapper
         behavior={ Platform.OS === 'ios' ? 'padding' : 'height' }
-        keyboardVerticalOffset={Platform.select({ ios: 0, android: 600 })}
+        keyboardVerticalOffset={Platform.select({ ios: 0, android: 500 })}
       >
-        <Content>
-          <Form>
-            <Header>
-              <Emoji>{ isFilled ? '😍' : '😃' }</Emoji>
-              <Title>Como podemos{'\n'} chamar você ?</Title>
-            </Header>
-            <Input
-              placeholder="Digite seu nome"
-              onChangeText={ handleInputChange }
-              onBlur={ handleInputBlur }
-              onFocus={ handleInputFocus }
-              isFocused={ isFocused }
-              isFilled={ isFilled }
-            />
-            <Button title="Confirmar"/>
-          </Form>
-        </Content>
+        <TouchableWithoutFeedback onPress={ Keyboard.dismiss }>
+          <Content>
+            <Form>
+              <Header>
+                <Emoji>{ isFilled ? '😍' : '😃' }</Emoji>
+                <Title>Como podemos{'\n'} chamar você ?</Title>
+              </Header>
+              <Input
+                placeholder="Digite seu nome"
+                onChangeText={ handleInputChange }
+                onBlur={ handleInputBlur }
+                onFocus={ handleInputFocus }
+                isFocused={ isFocused }
+                isFilled={ isFilled }
+              />
+              <Button
+                title="Confirmar"
+                onPress={ handleNavigationToConfirmation }
+              />
+            </Form>
+          </Content>
+        </TouchableWithoutFeedback>
       </Wrapper>
     </Container>
   )

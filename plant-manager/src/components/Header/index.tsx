0a12abafd6e0730ad491f74460b0c52d4406e64d
Gui-dev <adrianon10@gmail.com>
2021-06-05 03:52:20 -0300

    feat: add search AsyncStorage user

--- plant-manager/src/components/Header/index.tsx
+++ plant-manager/src/components/Header/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/core'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { Container, Info, Hello, Name, Image } from './style'
 
 export const Header: React.FC = () => {
+  const { navigate } = useNavigation()
+  const [username, setUsername] = useState<string | null>()
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const name = await AsyncStorage.getItem('@plantmanager:user')
+
+      if (!name) {
+        navigate('UserIdentification')
+      }
+
+      setUsername(name)
+    }
+    loadUser()
+  }, [username])
+
+  console.log(username)
+
   return (
     <Container>
       <Info>
         <Hello>Olá,</Hello>
-        <Name>Gui Silva</Name>
+        <Name>{ username }</Name>
       </Info>
       <Image source={{ uri: 'https://github.com/Gui-dev.png' }} />
     </Container>

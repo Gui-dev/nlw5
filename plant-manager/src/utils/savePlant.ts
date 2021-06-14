790470f48331c922132614804b61ac80f8a9c8c1
Gui-dev <adrianon10@gmail.com>
2021-06-14 03:15:30 -0300

    refactor: add on function savePlant an else

--- plant-manager/src/utils/savePlant.ts
+++ plant-manager/src/utils/savePlant.ts
@@ -33,13 +33,11 @@ export const savePlant = async (plant: IPlantProps): Promise<void> => {
     if (repeat_every === 'week') {
       const interval = Math.trunc(7 / times)
       nextTime.setDate(now.getDate() + interval)
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1)
     }
 
-    // else {
-    //   nextTime.setDate(nextTime.getDate() + 1)
-    // }
-
-    const seconds = Math.abs(Math.ceil((now.getTime() - nextTime.getTime()) / 1000))
+    const seconds = Math.abs(Math.ceil(now.getTime() - nextTime.getTime()) / 1000)
 
     const notificationId = await Notifications.scheduleNotificationAsync({
       content: {
